0d8d2b64d4a487fe38febd4e75a427f6cfaca63b
Dinne <e028148@corp.mastercard.org>
2016-09-26 00:26:09 -0500

    final version

--- app.js
+++ app.js
@@ -1,86 +1,47 @@
     (function () {
       "use strict";
+      angular.module("ShoppingListCheckOff", [])
+          .controller("BuyListController", BuyListController)
+          .controller("BoughtListController", BoughtListController)
+          .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
+      BuyListController.inject= ['ShoppingListCheckOffService'];
+      function BuyListController(ShoppingListCheckOffService){
+            var showbuylist= this;
 
-      var shoppingList1= ["milk","chapathi","dal","curd"];
-      var shoppingList2=[
-        {
-          name: "apple",
-          quantity: "2"
-        },
-        {
-          name:"orange",
-          quantity: "5"
-        },
-        {
-          name: "cherry",
-          quantity: "15"
-        }
-      ];
+            showbuylist.buyList = ShoppingListCheckOffService.getbuyitems();
+              showbuylist.additem= function (itemindex){
+                ShoppingListCheckOffService.addtoboughtlist(itemindex);
+              };
+              showbuylist.errormessage="Everything is bought!";
 
-      angular.module("DIApp", [])
-          .controller("DIController", DIController)
-          .filter("custom", CustomFactory)
-          .filter("check",CheckFactory);
-          DIController.inject= ['$scope','customFilter'];
-        function DIController( $scope, customFilter ){
-        $scope.name = "Eshwar"; //$filter('uppercase')("Eshwar");
-      //  $scope.FullName ="";
-        $scope.namevalue="0.45"; //$filter('currency')("0.45","$$","3");
-        $scope.shoppingList1 = shoppingList1;
-        $scope.shoppingList2  = shoppingList2;
+      }
 
-        $scope.sayhello = function () {
-          var msg = "I like tea and swwetts";
-          msg = customFilter(msg);
-          return msg;
-        };
-
-        $scope.showNumberofWatchers = function () {
-        //  alert("insidewatchers func");
-          console.log("# of watchers: ", + $scope.$$watchersCount);
-        };
-        $scope.setFullName = function () {
-        //  alert("insidewatchers func");
-        $scope.FullName = $scope.name +" "+"Dinne";
-        };
-
-        $scope.logFullName = function () {
-        //  alert("insidewatchers func");
-          console.log("log FullName: ", + $scope.FullName);
-        };
-        $scope.logname = function () {
-        //  alert("insidewatchers func");
-          console.log("log Name: ", + $scope.name);
-        };
-
-        $scope.addlist2 = function () {
-          // var itemname=$scope.newname;
-          // var itemquantity=$scope.newquantity;
-          var newitem=
-            {name:$scope.newname,
-              quantity: $scope.newquantity
-            };
-
-          shoppingList2.push(newitem);
-
-        };
+    BoughtListController.inject= ['ShoppingListCheckOffService'];
+    function BoughtListController(ShoppingListCheckOffService ){
+          var showboughtlist=this;
+        showboughtlist.boughtList = ShoppingListCheckOffService.getboughtitems();
+         showboughtlist.emptymessage="Nothing bought yet.";
     }
 
-    function CustomFactory() {
-      return function(input) {
-        //alert("hi");
-        input = input ||"";
-        input=input.replace("like","love");
-        return input;
-      };
-    };
-    function CheckFactory() {
-      return function(input,target,replace) {
-        //alert("hi");
-        input = input ||"";
-        input=input.replace(target,replace);
-        return input;
-      };
-    };
+  function ShoppingListCheckOffService() {
+          var service= this;
+          var buyList=[
+                      { name: "cookies", quantity: "10"}, { name: "donuts", quantity: "20"},{ name: "milkybars",quantity: "30"},{ name:"lollypups",quantity: "10"},{ name: "snickers",quantity: "5"}
+                      ];
+
+        var boughtList= [];
+        service.getbuyitems= function () {
+
+           return buyList;
+        };
+        service.getboughtitems= function () {
+                  return boughtList;
+        };
+        service.addtoboughtlist= function(itemindex){
+          boughtList.push(buyList[itemindex]);
+          buyList.splice(itemindex,1);
+        };
+  }
+
     })();
